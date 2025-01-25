e8c52976151689ffdabeb4571d61a8495861fba2
23f1000668 <23f1000668@ds.study.iitm.ac.in>
2025-01-25 19:24:02 +0530

    Created database schema for users, subjects, quizzes, and scores

---
+++ models/models.py
@@ -0,0 +1,69 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db=SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    full_name = db.Column(db.String(100), nullable=False)
+    qualification = db.Column(db.String(50))
+    dob = db.Column(db.Date)
+    college = db.Column(db.String(100))
+    scores = db.relationship('Score', back_populates='user')
+
+class Subject(db.Model):
+    __tablename__ = "subjects"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text)
+    remarks = db.Column(db.Text)
+    chapters = db.relationship('Chapter', back_populates='subject')
+
+class Chapter(db.Model):
+    __tablename__ = "chapters"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text)
+    remarks = db.Column(db.Text)
+    subject = db.relationship('Subject', back_populates='chapters')
+    quizzes = db.relationship('Quiz', back_populates='chapter')
+
+class Quiz(db.Model):
+    __tablename__ = "quizzes"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapters.id'), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text)
+    remarks = db.Column(db.Text)
+    date_of_quiz = db.Column(db.Date)
+    chapter = db.relationship('Chapter', back_populates='quizzes')
+    questions = db.relationship('Question', back_populates='quiz')
+    scores = db.relationship('Score', back_populates='quiz')
+
+class Question(db.Model):
+    __tablename__ = "questions"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quizzes.id'), nullable=False)
+    question_statement = db.Column(db.Text, nullable=False)
+    choice1 = db.Column(db.Text, nullable=False)
+    choice2 = db.Column(db.Text, nullable=False)
+    choice3 = db.Column(db.Text)
+    choice4 = db.Column(db.Text)
+    correct_answer = db.Column(db.Integer, nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    quiz = db.relationship('Quiz', back_populates='questions')
+
+class Score(db.Model):
+    __tablename__ = "scores"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quizzes.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    total_score = db.Column(db.Integer, nullable=False)
+    quiz = db.relationship('Quiz', back_populates='scores')
+    user = db.relationship('User', back_populates='scores')
