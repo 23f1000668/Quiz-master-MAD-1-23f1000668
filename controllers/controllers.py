6120ce8112fe8f2c8e1caa5dcfae6f8ee0dec2a0
23f1000668 <23f1000668@ds.study.iitm.ac.in>
2025-03-08 17:55:57 +0530

    today work

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -7,7 +7,9 @@ from datetime import datetime
 
 controllers=Blueprint('controllers',__name__)
 def check_role(role):
-    pass
+    if 'user_id' in session and session['role']==role:
+        return True
+    return False
 #home page which would redirect to user dashboard which the user is logined or takes to register page
 @controllers.route('/')
 def home():
@@ -43,6 +45,9 @@ def register():
         qualification = request.form['qualification']
         dob = datetime.strptime(request.form['dob'], '%Y-%m-%d').date()
         college = request.form['college']
+        if User.query.filter_by(email=email).first():
+            flash("Email already exists")
+            return render_template('register.html')
         new_user=User(username=username,email=email,password=password,full_name=full_name,qualification=qualification,dob=dob,college=college,roles="user")
         db.session.add(new_user)
         db.session.commit()
@@ -70,8 +75,72 @@ def login():
             flash('Login failed. Check your credentials and try again')
     return render_template('login.html')
 
+#logout page
+@controllers.route('/logout')
+def logout():
+    session.clear()
+    flash('You have been looged out.')
+    return redirect(url_for('controllers.home'))
+#user dashboard
 @controllers.route('/user/dashboard')
 def user_dashboard():
     if not check_role('user'):
-        return render_template('userdashboard')
+        flash('Access denied.You must be logged in as a user')
+        return redirect(url_for('controllers.login'))
+    return render_template('user_dashboard.html')
+
+#admin dashboard
+@controllers.route("/admin/dashboard")
+def admin_dashboard():
+    if not check_role('admin'):
+        flash('Access denied.Your not allowed here')
+        return redirect(url_for('controllers.login'))
+    subjects=Subject.query.all()
+    users=User.query.all()
+    return render_template('admin_dashboard.html',subjects=subjects,users=users)
+
+@controllers.route('/admin/quiz/show')
+def quiz_show():
+    if not check_role('admin'):
+        flash('You not allowed here')
+        return redirect(url_for('controllers.login'))
+    quiz=Quiz.query().all()
+    return render_template('admin_quiz.html',quiz=quiz)
+
+@controllers.route('/admin/summary')
+def summary_show():
+    if not check_role('admin'):
+        flash('Your not allowed here')
+        return redirect(url_for('controllers.login'))
+    score=Score.query().all()
+    return render_template('summary_show.html',score=score)
+
+@controllers.route('/admin/subject/create',methods=['GET','POST'])
+def subjects_create():
+    if not check_role('admin'):
+        flash('Your are not allowed here')
+        return redirect(url_for('controllers.login'))
+    if request.method=='POST':
+        name=request.form['name']
+        description=request.form['description']
+        new_subject=request.form(name = name,description=description)
+        db.session.add(new_subject)
+        db.session.commit()
+        flash('Subject created successfully')
+        return redirect(url_for('controllers.admin_dashboard'))
+    return render_template('subject_create.html')
+
+@controllers.route('/admin/subject/<int:id>/edit',methods=['GET','POST'])
+def subject_edit(id):
+    if not check_role('admin'):
+        flash('You are not allowed here')
+        return redirect(url_for('contollers.login'))
+    subject=Subject.query.get_or_404(id)
+    if request.method=='POST':
+        subject.name=request.form['name']
+        subject.description=request.form['description']
+        db.session.commit()
+        flash('Subject updated successfully!')
+        return redirect(url_for('controllers.admin_dashboard'))
+    return render_template('subject_edit.html',subject=subject)
 
